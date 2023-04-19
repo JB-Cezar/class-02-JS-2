ff0e0b18095c8e794977ef7aa98af320e9742ca2
João Bento <jbcrescencio1@gmail.com>
2023-04-18 18:56:19 -0700

    JB user class updated

--- js/main.js
+++ js/main.js
@@ -21,3 +21,19 @@ jQuery.extend({
     }
 });
 let doc = $.getData(dataFileCsv)
+
+function splitRow(bigString){
+    let row = bigString.split("\n");
+    for (let j = 1; j < row.length-1; j++){
+        let col = row[j].split(",");
+        let user = new User(
+            col[0],
+            col[1],
+            col[2],
+            col[3],
+            col[4],
+            col[5]
+        )
+        userList.set()
+    }
+}
--- js/user.js
+++ js/user.js
@@ -1,18 +1,54 @@
 class User{
-#id
-#first_manme
-#last_name
-#email
-#counter
-constructor(id, frist_name,last_name,email,country){
-    this.#id = id
-    this.#first_manme = first_manme
-    this.#last_name = last_name
-    this.#email = email
-    this.#counter = counter
-}
-getId(){
-    return this. #id
-}
+    #id
+    #first_manme
+    #last_name
+    #email
+    #counter
+    constructor(id, frist_name,last_name,email,country){
+        this.#id = id
+        this.#frist_name = frist_name
+        this.#last_name = last_name
+        this.#email = email
+        this.#country = country
+    }
+    getId(){
+        return this. #id;
+    }
 
-}
+    setId(id){
+        return this.#id = id;
+    }
+
+    getFristName(){
+        return this.#frist_name
+    }
+
+    setFristName(frist_name){
+        return this.#frist_name = frist_name
+    }
+
+    getLastName(){
+        return this.#last_name
+    }
+
+    setLastName(last_name){
+        return this.#last_name = last_name
+    }
+
+    getEmail(){
+        return this.#email
+    }
+
+    setEmail(email){
+        return this.#email = email
+    }
+
+    getCountry(){
+        return this.#country
+    }
+
+    setCountry(country){
+        return this.#country = country
+    }
+    // joão, katsuxi aya
+} 
